f17112eb8dbb227a9dadbe007c2fd9da9fca268e
LOKMANFAVZAN <156150236+LOKMANFAVZAN@users.noreply.github.com>
2024-01-10 08:53:30 +0700

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,8 @@
+
+
+public class BankAccount {
+    BankAccount myAccount = new BankAccount("122356570", 320.0, "LOKMAN FAVZAN", "FavZan.IT.ST@gmail.com", "087213108");
+
+    public BankAccount(String 122356570, double v, String lokmanFavzan, String LOKMAN FAVZAN, String FavZan.IT.ST@gmail.com) {
+    }
+}
